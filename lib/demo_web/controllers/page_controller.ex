ab995dd84f6af5f239a8587f4eb47ff704d53c6e
kw7oe <choongkwern@hotmail.com>
2021-07-24 16:43:44 +0800

    Add simple ecto query to GET /

--- lib/demo_web/controllers/page_controller.ex
+++ lib/demo_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule DemoWeb.PageController do
   use DemoWeb, :controller
 
   def index(conn, _params) do
+    Demo.Repo.query!("select current_timestamp;")
     render(conn, "index.html")
   end
 end
